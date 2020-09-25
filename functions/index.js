50264bf5aea96891095cede304c71282c569b86d
@EzraMoussa <ezra.moussa@gmail.com>
2020-09-24 19:50:43 -0700

    modified functions

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,35 @@
-/*eslint-disable */
+// /*eslint-disable */
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+const db = admin.firestore();
 
 
 exports.addUser = functions.auth.user().onCreate((user) => {
   console.log(user);
   console.log(user.email);
+
+  const uid = user.uid;
+  const userFields = {
+    email: user.email,
+    name: user.displayName,
+    total_txs: 0,
+  }
+  console.log(userFields);
+
+  //add uid to the users collection
+  async function setData() {
+    await db.collection('users').doc(uid).set(userFields);
+  }
+  setData();
+  return 0;
+
 });
 
-/*eslint-enable */
+
+
+// /*eslint-enable */
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
