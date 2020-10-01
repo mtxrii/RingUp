bb7152cb97c33ab01daf8b40b538445117bb6966
Enrique <edavalos@ucsc.edu>
2020-09-30 23:45:49 -0700

    Added timeline
    wip

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="404">
-    <img src='../assets/undraw/404.svg' alt='404' width="40%" style="padding-top: 50px;"/>
+    <img src='../assets/undraw/404.svg' alt='404' width="40%" style="padding-top: 120px;"/>
 
     <div class="ru-head ru-sub" style="margin-bottom: 350px">Sorry, theres nothing here.</div>
   </div>
